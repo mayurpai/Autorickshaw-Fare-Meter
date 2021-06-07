a8407145537099793323ae1cd378fbf370dbe448
mayurpai <mayur5pai@gmail.com>
2021-06-07 10:31:46 +0530

    Trip Details

---
+++ app/src/main/java/com/example/afm/ui/home/TripFragment.java
@@ -0,0 +1,35 @@
+package com.example.afm.ui.home;
+
+import android.arch.lifecycle.ViewModelProvider;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.afm.R;
+
+public class TripFragment extends Fragment {
+
+    private TripViewModel mViewModel;
+
+    public static TripFragment newInstance() {
+        return new TripFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.trip_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this, new ViewModelProvider.NewInstanceFactory()).get(TripViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
